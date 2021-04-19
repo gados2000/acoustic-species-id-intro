5979ca36b4c53f02e15df0c0ceddba4836b247ff
Gadi R <Gados2000@gmail.com>
2021-04-18 20:56:38 -0700

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from dateutil.parser import parse
 
 def SRS(filepath):
     df = pd.read_csv(filepath, dtype={'AudioMothID' : str, 'StartDateTime' : str, 'Artist' : str})
@@ -7,8 +8,10 @@ def SRS(filepath):
     df.drop(df[df.Duration == "NA"].index, inplace=True)
     df.drop(df[df.Duration < 60.00025].index, inplace=True)
     df.drop(df[df.AudioMothCode.isin(["AM-21", "AM-19", "AM-8", "AM-28", "AM-18"])].index, inplace=True)
-    df['StartDateTime'] = pd.to_datetime(df["StartDateTime"], format="%d.%m.%Y %H:%M")
-    df['Hour'] = df['StartDateTime'].dt.hour.tolist()
+
+    df['Comment'] = pd.to_datetime(df['Comment'].str.extract(' (\d{2}:\d{2}:\d{2} \d{2}\/\d{2}\/\d{4}) ').squeeze(), format='%H:%M:%S %d/%m/%Y')
+
+    df['Hour'] = df['Comment'].dt.hour.tolist()
     df = df.groupby(['AudioMothCode', 'Hour']).sample(1, random_state=1).reset_index()
     if(df.empty):
         return False
